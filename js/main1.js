eb1982e25d787d5945f974b9bb908908bf4e8777
zdooley1 <zdooley1@uw.edu>
2022-04-29 14:10:42 -0700

    commit all docs

---
+++ js/main1.js
@@ -0,0 +1,98 @@
+// initialize basemmap
+mapboxgl.accessToken =
+'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/light-v10', // style URL
+zoom: 3, // starting zoom
+center: [-100, 40] // starting center
+});
+
+// load data and add as layer
+async function geojsonFetch() {
+let response = await fetch('assets/covid_rates_2020_simplified.geojson');
+let stateData = await response.json();
+
+map.on('load', function loadingData() {
+    map.addSource('covid_rates_2020_simplified', {
+        type: 'geojson',
+        data: stateData
+    });
+
+    map.addLayer({
+        'id': 'covid_rates_2020_simplified-layer',
+        'type': 'fill',
+        'source': 'covid_rates_2020_simplified',
+        'paint': {
+            'fill-color': [
+                'step',
+                ['get', 'rates'],
+                '#FFEDA0',   // stop_output_0
+                50,          // stop_input_0
+                '#FED976',   // stop_output_1
+                100,          // stop_input_1
+                '#FEB24C',   // stop_output_2
+                150,          // stop_input_2
+                '#FD8D3C',   // stop_output_3
+                200,         // stop_input_3
+                '#FC4E2A',   // stop_output_4
+                250,         // stop_input_4
+                '#E31A1C',   // stop_output_5
+                300,         // stop_input_5
+                '#BD0026',   // stop_output_6
+            ],
+            'fill-outline-color': '#BBBBBB',
+            'fill-opacity': 0.7,
+        }
+    });
+
+    const layers = [
+        '0-49',
+        '50-99',
+        '100-149',
+        '150-199',
+        '200-249',
+        '250-299',
+        '300 and more'
+    ];
+    const colors = [
+        '#FFEDA070',
+        '#FED97670',
+        '#FEB24C70',
+        '#FD8D3C70',
+        '#FC4E2A70',
+        '#E31A1C70',
+        '#BD002670'
+    ];
+
+    // create legend
+    const legend = document.getElementById('legend');
+    legend.innerHTML = "<b>Covid-19 Rates<br>(pos tests/total tests)</b><br><br>";
+
+
+    layers.forEach((layer, i) => {
+        const color = colors[i];
+        const item = document.createElement('div');
+        const key = document.createElement('span');
+        key.className = 'legend-key';
+        key.style.backgroundColor = color;
+
+        const value = document.createElement('span');
+        value.innerHTML = `${layer}`;
+        item.appendChild(key);
+        item.appendChild(value);
+        legend.appendChild(item);
+    });
+});
+
+map.on('mousemove', ({point}) => {
+    const covid = map.queryRenderedFeatures(point, {
+        layers: ['covid_rates_2020_simplified-layer']
+    });
+    document.getElementById('text-escription').innerHTML = covid.length ?
+        `<h3>${covid[0].properties.county}</h3><p><strong><em>${covid[0].properties.rates}</strong> pos cases/total cases</em></p>` :
+        `<p>Hover over a county!</p>`;
+});
+}
+
+geojsonFetch();
---
+++ js/main2.js
@@ -0,0 +1,108 @@
+mapboxgl.accessToken =
+'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+let map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/light-v10',
+zoom: 3, // starting zoom
+center: [-100, 40] // starting center
+});
+
+const grades = [10, 1000, 50000, 100000, 1000000], 
+    colors = ['rgb(218,218,235)', 'rgb(188,189,220)', 'rgb(158,154,200)', 'rgb(117,107,177)', 'rgb(84,39,143)'], 
+    radii = [3, 6, 9, 12, 15];
+
+//load data to the map as new layers.
+//map.on('load', function loadingData() {
+map.on('load', () => { //simplifying the function statement: arrow with brackets to define a function
+
+// when loading a geojson, there are two steps
+// add a source of the data and then add the layer out of the source
+map.addSource('covid_counts_2020', {
+    type: 'geojson',
+    data: 'assets/covid_counts_2020.geojson'
+});
+
+map.addLayer({
+        'id': 'covid_counts_2020-point',
+        'type': 'circle',
+        'source': 'covid_counts_2020',
+        'minzoom': 3,
+        'paint': {
+            // increase the radii of the circle as the zoom level and dbh value increases
+            'circle-radius': {
+                'property': 'cases',
+                'stops': [
+                    [{
+                        zoom: 5,
+                        value: grades[0]
+                    }, radii[0]],
+                    [{
+                        zoom: 5,
+                        value: grades[1]
+                    }, radii[1]],
+                    [{
+                        zoom: 5,
+                        value: grades[2]
+                    }, radii[2]],
+                    [{
+                        zoom: 5,
+                        value: grades[3]
+                    }, radii[3]],
+                    [{
+                        zoom: 5,
+                        value: grades[4]
+                    }, radii[4]]
+                ]
+            },
+            'circle-color': {
+                'property': 'cases',
+                'stops': [
+                    [grades[0], colors[0]],
+                    [grades[1], colors[1]],
+                    [grades[2], colors[2]],
+                    [grades[3], colors[3]],
+                    [grades[4], colors[4]]
+                ]
+            },
+            'circle-stroke-color': 'grey',
+            'circle-stroke-width': 1,
+            'circle-opacity': 0.6
+        }
+    },
+    'waterway-label'
+);
+
+
+// click on tree to view cases in a popup
+map.on('click', 'covid_counts_2020-point', (event) => {
+    new mapboxgl.Popup()
+        .setLngLat(event.features[0].geometry.coordinates)
+        .setHTML(`<strong>County:</strong> ${event.features[0].properties.county} <br> <strong>Cases:</strong> ${event.features[0].properties.cases}`)
+        .addTo(map);
+});
+
+});
+
+
+// create legend
+const legend = document.getElementById('legend');
+
+//set up legend grades and labels
+var labels = ['<strong>Cases</strong>'], vbreak;
+//iterate through grades and create a scaled circle and label for each
+for (var i = 0; i < grades.length; i++) {
+vbreak = grades[i];
+// you need to manually adjust the radius of each dot on the legend 
+// in order to make sure the legend can be properly referred to the dot on the map.
+dot_radii = 2 * radii[i];
+labels.push(
+    '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radii +
+    'px; height: ' +
+    dot_radii + 'px; "></i> <span class="dot-label" style="top: ' + dot_radii / 2 + 'px;">' + vbreak +
+    '</span></p>');
+
+}
+const source =
+'<p style="text-align: right; font-size:10pt">Source: <a href="https://earthquake.usgs.gov/earthquakes/">USGS</a></p>';
+
+legend.innerHTML = labels.join('') + source;
